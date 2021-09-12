a355ad533c87d516d77afdee858628654096d2bf
Vlads <Vladsinitsa23@gmail.com>
2021-09-12 15:08:54 +0300

    Create new application

---
+++ src/main/java/com/example/projectCompany/ProjectCompanyApplication.java
@@ -0,0 +1,13 @@
+package com.example.projectCompany;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ProjectCompanyApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ProjectCompanyApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/projectCompany/ProjectCompanyApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.projectCompany;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ProjectCompanyApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
